1daa896c143f20e99a28f5edc6ad291c110011d7
Marina Katz <marina.katz@vitec.com>
2025-08-05 16:10:35 +0300

    Add project files.

---
+++ Data/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data;
+
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options) { }
+
+    public DbSet<TodoItem> Todos => Set<TodoItem>();
+}
